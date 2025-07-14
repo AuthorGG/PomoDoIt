2bd3fc1ede4cf8d842d8d2c4fc667dacbd203dec
Carlos Curiel <ccurielramos@gmail.com>
2025-07-14 18:50:34 +0200

    improvements and queries for responsiveness

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -109,7 +109,7 @@ function Timer({ initialMinutes }) {
           initial={{ y: 10, opacity: 0 }}
           animate={{ y: 0, opacity: 1 }}
           exit={{ y: -10, opacity: 0 }}
-          transition={{ duration: 0.3 }}
+          transition={{ duration: 0.5 }}
         >
           {formatTime(secondsLeft)}
         </motion.h2>
